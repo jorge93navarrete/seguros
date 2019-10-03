a1e952fe509eb852939d116923bfe834b3d7e6bc
jorge navarrete <jorge1793.navarrete@gmail.com>
2019-10-03 16:05:05 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require('electron')
+let win
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  // Abre las herramientas de desarrollo (DevTools).
+  win.webContents.openDevTools()
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+  // Emitido cuando la ventana es cerrada.
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+// Este método será llamado cuando Electron haya terminado
+// la inicialización y esté listo para crear ventanas del navegador.
+// Algunas APIs pueden usarse sólo después de que este evento ocurra.
+app.on('ready', createWindow)
+
+// Sal cuando todas las ventanas hayan sido cerradas.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // En macOS es común volver a crear una ventana en la aplicación cuando el
+  // icono del dock es clicado y no hay otras ventanas abiertas.
+  if (win === null) {
+    createWindow()
+  }
+})
+
